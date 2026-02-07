326db62bf89f57dbac41b653bad4cd55653aff05
Adwaina <arichard02@aol.com>
2026-02-07 16:58:33 -0500

    reorginized code into functions. draft version of detail screen

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 console.log("Script.js is working");
+let countries;
+
 
 // toggle light/dark mode
 function toggleTheme() {
@@ -7,7 +9,7 @@ function toggleTheme() {
 }
 
 // anytime you use a promise it has to be in an async function
-// await = do not move on to the next statement until the 
+// await = do not move on to the next statement until the
 // promise has finished its statement and given you its data
 // get information from api
 async function getCountryData() {
@@ -16,9 +18,37 @@ async function getCountryData() {
     "https://restcountries.com/v3.1/all?fields=name,population,region,capital,flags",
   );
   // step 2 - then retrive the response data (payload)
-  let countries = await response.json();
+  countries = await response.json();
   // step 3 - do something with the data
   outputCountries(countries);
+}
+
+// the job of this function is to create a html card from the country json object
+function createCountryCard(countryObj, i) {
+  let htmlContent = `
+        <div class="card" onclick="showCountryDetail(${i})">
+              <img src= "${countryObj.flags.png}" alt="${countryObj.flags.alt}"/>
+              <h3>${countryObj.name.common}</h3>
+              <p>${countryObj.population}</p>
+              <p>${countryObj.region}</p>
+              <p>${countryObj.capital[0]}</p>
+            </div>
+        `;
+  return htmlContent;
+}
+
+function createCountryDetailDisplay(countryObj) {
+  let htmlContent = `
+        <div>
+        <button onclick="backButton()">Back</button>
+              <img src= "${countryObj.flags.png}" alt="${countryObj.flags.alt}"/>
+              <h3>${countryObj.name.common}</h3>
+              <p>${countryObj.population}</p>
+              <p>${countryObj.region}</p>
+              <p>${countryObj.capital[0]}</p>
+            </div>
+        `;
+  return htmlContent;
 }
 
 // this function requires that you pass it a list of countries
@@ -27,22 +57,25 @@ async function getCountryData() {
 function outputCountries(countries) {
   let container = document.getElementById("output");
   for (let i = 0; i < countries.length; i++) {
-    let htmlContent = `
-        <div class="card" onclick="showCountryDetail(${i})">
-              <img src= "${countries[i].flags.png}" alt="${countries[i].flags.alt}"/>
-              <h3>${countries[i].name.common}</h3>
-              <p>${countries[i].population}</p>
-              <p>${countries[i].region}</p>
-              <p>${countries[i].capital[0]}</p>
-            </div>
-        `;
+    let htmlContent = createCountryCard(countries[i], i);
+    console.log("outputing the next country to the dom", htmlContent)
     container.insertAdjacentHTML("beforeend", htmlContent);
   }
 }
 
-function showCountryDetail (i) {
-console.log(i);
+function showCountryDetail(i) {
+  console.log(i);
+   let container = document.getElementById("output");
+   container.innerHTML = "";
+    let htmlContent = createCountryDetailDisplay(countries[i], i);
+     container.insertAdjacentHTML("beforeend", htmlContent);
+}
+
+function backButton() {
+   let container = document.getElementById("output");
+   container.innerHTML = "";
+   outputCountries(countries);
 }
 
 // invoke/run the fetch function when the page first loads(getCountryData)
-getCountryData();  
+getCountryData();
